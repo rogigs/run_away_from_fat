914a5e8ff7e59bdd1e274dc01d23fa94498bec64
Igor Gomes <igorgomes.13ig@gmail.com>
2021-04-28 20:47:18 -0300

    Adding class Character

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+from character import Character
+
 pygame.init()
 SCREEN_SIZE = (800, 600)
-screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
+SCREEN = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+    # ESSE TRECHO DE CÓDIGO DEVE IR PARA A INTERFACE
+    character = Character('Usain', SCREEN)
+    character.draw(50, 100)
 
     pygame.display.update()
