0dec0b09cebdd221936e9f8361c96a86b4125190
Bruce Neco <bruce.neco3@outlook.com>
2021-05-03 23:43:06 -0300

    add backbutton and title to new game

--- model/new_game.py
+++ model/new_game.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from utils.position import in_bounds
 from utils.data_manipulation import Data
+from config import NEW_GAME_IMAGES_PATH
 
 
 class NewGame:
@@ -11,16 +12,37 @@ class NewGame:
         self.screen = screen
         self.bg = bg
         self.on = True
-
-        self.usaim = pygame.transform.scale(pygame.image.load("img/new_game/usaim.png"),
-                                            (int(self.width / 5), int(self.height / 5 * 2)))
+        self.usaim, self.radcliffe, self.backbutton = None, None, None
+        
+        self.reset_imgs()
         self.usaim_bounds = [
-            (self.width / 5, self.height / 5 * 2),
-            (self.width / 5 + self.usaim.get_width(), self.height / 5 * 2 + self.usaim.get_height())
+            (self.width // 2 - self.usaim.get_width() - 32, self.height // 2 - 32),
+            (self.width // 2 - 32, self.height // 2 - 32 + self.usaim.get_height())
+        ]
+        self.radcliffe_bounds = [
+            (self.width // 2 + 32, self.height // 2 - 32),
+            (self.width // 2 + self.radcliffe.get_width() + 32, self.height // 2 - 32 + self.radcliffe.get_height())
+        ]
+
+        self.backbutton_bounds = [
+            (16, 16),
+            (16 + self.backbutton.get_width(), 16 + self.backbutton.get_height())
         ]
 
     def show_usaim(self):
         self.screen.blit(self.usaim, self.usaim_bounds[0])
+
+    def show_radcliffe(self):
+        self.screen.blit(self.radcliffe, self.radcliffe_bounds[0])
+
+    def show_backbutton(self):
+        self.screen.blit(self.backbutton, self.backbutton_bounds[0])
+
+    def show_title(self):
+        img = pygame.image.load(NEW_GAME_IMAGES_PATH + "selecione-personagem.png").convert_alpha()
+        img = pygame.transform.scale(img, (self.width // 3 * 2, 180))
+        render_img = img.get_rect().center = self.width // 2 - img.get_width() // 2, 32
+        self.screen.blit(img, render_img)
 
     def show(self):
         self.on = True
@@ -35,16 +57,36 @@ class NewGame:
                     self.process_mouseup(pygame.mouse.get_pos())
             self.screen.blit(self.bg, (0, 0))
             self.show_usaim()
+            self.show_radcliffe()
+            self.show_backbutton()
+            self.show_title()
             pygame.display.update()
 
     def process_mousedown(self, pos):
         if in_bounds(pos, self.usaim_bounds):
-            self.usaim = pygame.transform.scale(pygame.image.load("img/new_game/usaim-pressed.png"),
-                                   (int(self.width / 5), int(self.height / 5 * 2)))
+            self.usaim = pygame.transform.scale(pygame.image.load(NEW_GAME_IMAGES_PATH + "usaim-pressed.png"),
+                                                (int(self.width / 6), int(self.height / 13 * 6)))
+        if in_bounds(pos, self.radcliffe_bounds):
+            self.radcliffe = pygame.transform.scale(pygame.image.load(NEW_GAME_IMAGES_PATH + "radcliffe-pressed.png"),
+                                                    (int(self.width / 6), int(self.height / 13 * 6)))
+        if in_bounds(pos, self.backbutton_bounds):
+            self.backbutton = pygame.transform.scale(pygame.image.load(NEW_GAME_IMAGES_PATH + "voltar-pressed.png"),
+                                                     (80, 80))
 
     def process_mouseup(self, pos):
+        self.reset_imgs()
         if in_bounds(pos, self.usaim_bounds):
-            self.usaim = pygame.transform.scale(pygame.image.load("img/new_game/usaim.png"),
-                                   (int(self.width / 5), int(self.height / 5 * 2)))
             Data.create_new_person("U")
             self.on = False
+        if in_bounds(pos, self.radcliffe_bounds):
+            Data.create_new_person("R")
+            self.on = False
+        if in_bounds(pos, self.backbutton_bounds):
+            self.on = False
+
+    def reset_imgs(self):
+        self.usaim = pygame.transform.scale(pygame.image.load(NEW_GAME_IMAGES_PATH + "usaim.png"),
+                                            (int(self.width / 6), int(self.height / 13 * 6)))
+        self.radcliffe = pygame.transform.scale(pygame.image.load(NEW_GAME_IMAGES_PATH + "radcliffe.png"),
+                                                (int(self.width / 6), int(self.height / 13 * 6)))
+        self.backbutton = pygame.transform.scale(pygame.image.load(NEW_GAME_IMAGES_PATH + "voltar.png"), (80, 80))
