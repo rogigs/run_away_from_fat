42c6858eb25bb641e1fa4452c749ae47a5fdd316
Bruce Neco <bruce.neco3@outlook.com>
2021-05-05 20:13:47 -0300

    Merge PR#2

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-MENU_IMAGES_PATH = "assets\\img\menu\\"
-NEW_GAME_IMAGES_PATH = "assets\\img\\new_game\\"
-IMAGES_PATH = "assets\\img\\"
-FONT_PATH = "assets\\font\\"
-SOUNDS_PATH = "assets\\sounds\\"
-=======
-
 sep = "/"
 MENU_IMAGES_PATH = "assets" + sep + "img" + sep + "menu" + sep
 NEW_GAME_IMAGES_PATH = "assets" + sep + "img" + sep + "new_game" + sep
@@ -14,4 +6,3 @@ FONT_PATH = "assets" + sep + "font" + sep
 HUD_PATH = "assets" + sep + "img" + sep + "hud" + sep
 PAUSE_PATH = "assets" + sep + "img" + sep + "pause" + sep
 SOUNDS_PATH = "assets" + sep + "sounds" + sep
->>>>>>> e80610b5045cf5c1930613b18b88cd2c5575fff0
